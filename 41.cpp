103ba0c678261503c751eb0c60c8cfdbd93de4ed
priusri <romilawork@gmail.com>
2025-06-19 05:58:17 +0530

    Create 41.cpp

---
+++ 41.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n=nums.size();
+        int maxVal=0;
+       vector <int> freq(n+2 , 0);
+
+       for(int i=0 ; i<n ;i++ ){
+          if( nums[i]>0 && nums[i]<= n+1){
+            freq[nums[i]]++;
+          }
+        }
+       for( int i=1 ; i< freq.size() ; i++){
+        if(freq[i]==0){
+            return i;
+        }
+       }
+       return 0;
+       
+    }
+};
